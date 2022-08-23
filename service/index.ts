7c2c994a5d0998ab5d9eb0a7826536dfcfd22ff9
amin soraya <aminsoraya@gmail.com>
2022-08-23 22:43:03 +0430

    modify https

--- service/index.ts
+++ service/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosInstance } from 'axios'
 
-const baseURL = 'http://service.dastshafa.ir/api/Product'
+const baseURL = 'https://service.dastshafa.ir/api/Product'
 const config: AxiosRequestConfig = {
   baseURL,
 }
